12d76e823f85818aadf236cf06deb4fd82a19079
Andrew Rowson <github@growse.com>
2013-12-09 15:45:57 +0000

    initial

---
+++ src/com/growse/android/Locator/MyActivity.java
@@ -0,0 +1,19 @@
+package com.growse.android.Locator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Intent i = new Intent(this, LocatorService.class);
+        startService(i);
+
+    }
+}
